cc4713b62f05cfa20818ec1f8763a30f907fb9a5
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-05 06:31:39 +0900

    ✨ 가로 스크롤 구현

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
@@ -8,12 +10,29 @@ export default function App() {
         <View style={styles.city}>
           <Text style={styles.cityName}>Suwon</Text>
         </View>
-        <View style={styles.weather}>
+        <ScrollView
+          pagingEnabled
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.weather}
+        >
           <View style={styles.day}>
             <Text style={styles.temp}>27</Text>
             <Text style={styles.description}>Sunny</Text>
           </View>
-        </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </ScrollView>
       </View>
       <StatusBar style="light" />
     </>
@@ -28,9 +47,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityName: { color: "gray", fontSize: 68, fontWeight: "600" },
-  weather: { flex: 3 },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: { color: "gray", marginTop: 50, fontSize: 178 },
